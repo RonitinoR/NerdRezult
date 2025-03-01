d641ab168ffd5374aa24cc96aab10d7ac0ffbb68
Sai Sindhu Muppaneni <114361721+saisindhu2698@users.noreply.github.com>
2025-02-28 17:54:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,22 @@ import Login from "./components/Login";
 import EmailLoginPage from "./components/Loginsetup/EmailLoginPage";
 import SignUp from "./components/Loginsetup/SignUp";
 import HomeScreen from "./components/Loginsetup/HomeScreen";
-<<<<<<< HEAD
 import Profile from "./components/Profile/ProfilePage";
 import ProjectPage from "./components/Projectscreen/projectpage";
-=======
->>>>>>> 039e131b3820b13067b03547123a73ac3b90f334
+
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
       <Route path="/" element={<Login />} />
-=======
         <Route path="/" element={<Login />} />
->>>>>>> 039e131b3820b13067b03547123a73ac3b90f334
         <Route path="/HomeScreen" element={<HomeScreen />} />
         <Route path="/email-login" element={<EmailLoginPage />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
         <Route path="/profile" element={<Profile />} />
         <Route path="/projects" element={<ProjectPage isPopup={false} />} />
-=======
->>>>>>> 039e131b3820b13067b03547123a73ac3b90f334
       </Routes>
     </Router>
   );
