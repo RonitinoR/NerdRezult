c3b310fc11adebd16934c0feef23a6d7ea2083a0
Sai Sindhu Muppaneni <114361721+saisindhu2698@users.noreply.github.com>
2025-03-05 13:29:42 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import EmailLoginPage from "./components/Loginsetup/PhoneLoginPage";
+import PhoneLoginPage from "./components/Loginsetup/PhoneLoginPage";
 import SignUp from "./components/Loginsetup/SignUp";
 import HomeScreen from "./components/Loginsetup/HomeScreen";
 import Profile from "./components/Profile/ProfilePage";
 import ProjectPage from "./components/Projectscreen/projectpage";
+import { AuthProvider } from './contexts/AuthContext';
+import OAuthCallback from './components/OAuthCallback';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<Login />} />
+    <AuthProvider>
+      <Router>
+        <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/HomeScreen" element={<HomeScreen />} />
-        <Route path="/phone-login" element={<PhoneLoginPage />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/projects" element={<ProjectPage isPopup={false} />} />
-      </Routes>
-    </Router>
+          <Route path="/" element={<Login />} />
+          <Route path="/HomeScreen" element={<HomeScreen />} />
+          <Route path="/phone-login" element={<PhoneLoginPage />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/projects" element={<ProjectPage isPopup={false} />} />
+          <Route path="/oauth-callback" element={<OAuthCallback />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
